de184a2e5e090eb38b06b14145bb61617fb7fce4
Young-in <daddy321@snu.ac.kr>
2018-07-24 11:58:27 +0900

    Chromosome 설명 변경 및 간단한 Visualization 추가

--- Chromosome.py
+++ Chromosome.py
@@ -1,13 +1,16 @@
 class Chromosome:
-    # trip : array of requests in order of visits (positive value: ride, negative value: drop off)
-    def __init__(self, trip):
-        self.trip = trip
+    # trips : an array of an array of requests in order of visits (positive value: ride, negative value: drop off)
+    # 하나의 유전자가 여러대의 셔틀의 경로를 모두 나타내고 있어야 될거 같아서 trip 보단 trips로 이차원 배열을 가지고 있는게 맞는거 같다
+    # 논문에 나와있는 remove one insert one 같은 변화들은 mutation으로 나타내는게 맞는거 같고,
+    # crossover는 두 유전자에서 각 셔틀이 어느 유전자의 trip을 선택할지 확률적으로 결정하면 될거 같다(물론 requests가 겹치지 않도록 후처리도 해야되고)
+    def __init__(self, trips):
+        self.trips = trips
         pass
 
     def __str__(self):
         pass
 
-    def mutation(self) :
+    def mutation(self):
         pass
 
     def crossover(self, chromo):
--- Simulator.py
+++ Simulator.py
@@ -1,8 +1,11 @@
 from DataGenerator import DataGenerator
+from Visualization import Visualization
 
 def main():
     DG = DataGenerator(n = 50, m = 10)
     print(DG)
+    V = Visualization()
+    V.drawPoints([coord[0] for coord in DG.stations], [coord[1] for coord in DG.stations], 'stations')
     pass
 
 if __name__ == "__main__": # execute when python Simulator.py executed
