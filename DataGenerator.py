37feee11b941ccc7c425a809ff9bbacec9b6e419
Young-in <daddy321@snu.ac.kr>
2018-07-24 18:36:48 +0900

    Data Generator 수정

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,6 +1,7 @@
 import Chromosome
 import Pool
 import random
+import math
 
 class DataGenerator:
     # n : the number of requests
@@ -9,22 +10,6 @@ class DataGenerator:
     # stations : locations of stations [tuple of 2 real numbers(x, y)]
     # T : the maximum time of the simulation
     def __init__(self, n = 1000, m = 20, T = 1440):
-        self.n = n
-        self.requests = []
-        for i in range(self.n) :
-            sta0 = sta1 = random.randrange(m)+1
-            while sta0 == sta1 :
-                sta1 = random.randrange(m)+1
-            t0 = t1 = random.randrange(T)
-            while t0 == t1 :
-                t1 = random.randrange(T)
-            if(t1 < t0) :
-                temp = t0
-                t0 = t1
-                t1 = temp
-            self.requests.append((t0, sta0, t1, sta1))
-        # To ensure two stations, times are different
-
         self.m = m
         self.stations = []
         for j in range(self.m) :
@@ -33,6 +18,29 @@ class DataGenerator:
                 sta = (random.random() * 100, random.random() * 100)
             self.stations.append(sta)
         # To ensure all stations are different
+        
+        self.n = n
+        self.requests = []
+        for i in range(self.n) :
+            # sta0 = sta1 = random.randrange(m)+1
+            # while sta0 == sta1 :
+            #     sta1 = random.randrange(m)+1
+            # t0 = t1 = random.randrange(T)
+            # while t0 == t1 :
+            #     t1 = random.randrange(T)
+            # if(t1 < t0) :
+            #     temp = t0
+            #     t0 = t1
+            #     t1 = temp
+
+            # new version without using loop
+            sta0 = random.randrange(m)+1
+            sta1 = (sta0 + random.randrange(m-1)) % m + 1
+            t0 = random.randrange(T)
+            t1 = (t0 + random.randrange(T-1) + 1) % T
+            if t1 < t0: t0, t1 = t1, t0
+            self.requests.append((t0, sta0, t1, sta1))
+        # To ensure two stations, times are different
         pass
 
     def __str__(self):
@@ -45,3 +53,6 @@ class DataGenerator:
         ret += "------------------------------------\n"
 
         return ret
+
+    def getDistance(self, x, y): # get euclidean distance between station x and station y
+        return math.sqrt((self.stations[x][0]-self.stations[y][0])**2+(self.stations[x][1]-self.stations[y][1])**2)
