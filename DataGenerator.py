c19f9dd62ef55b14a190b347f041e56384acd877
Young-in <daddy321@snu.ac.kr>
2018-07-27 23:19:47 +0900

    OTOC 추가 및 자잘한 수정

--- DataGenerator.py
+++ DataGenerator.py
@@ -25,13 +25,16 @@ class DataGenerator:
         self.requests = []
         for i in range(self.n) :
             # new version without using loop
-            sta0 = random.randrange(1, m+1)
+            sta0 = random.randrange(m)
             sta1 = (sta0 + random.randrange(1, m)) % m
+            # change index 1~m to 0~m-1 for easy access of dist[][]
             t0 = random.randrange(T)
             t1 = (t0 + random.randrange(T-1) + 1) % T
             if t1 < t0 : t0, t1 = t1, t0
             self.requests.append((t0, sta0, t1, sta1))
         # To ensure two stations, times are different
+
+        self.T = T
         pass
 
     def __str__(self):
@@ -46,13 +49,13 @@ class DataGenerator:
         return ret
 
     def getDists(self):
-        dists = []
-        n = len(self.stations) # number of sta
-        for i in range(n) :
-            for j in range(i, n) :
+        m = self.m # number of sta
+        dists = [[None] * m for i in range(m)]
+        for i in range(m) :
+            for j in range(m) :
                 d = self.getDistance(i, j)
                 dists[i][j] = d
-                dists[j][i] = d
+        # made code simpler
         return dists
 
 
@@ -60,11 +63,38 @@ class DataGenerator:
         return math.sqrt((self.stations[x][0]-self.stations[y][0])**2
                          +(self.stations[x][1]-self.stations[y][1])**2)
 
-    def getCost(self, chromo):
+    def getCost(self, chromo): # ## need to change this function: trip has a request number not a station number
         cost = 0
         for trip in chromo.trips :
             l = len(trip)
             for i in l-1 :
                 cost += self.dists[trip[i]][trip[i+1]]
         return cost
+
+    def generateOTOC(self):
+        requests = list(enumerate(self.requests[:]))
+        requests.sort(key = lambda request : request[1][2])
+
+        trips = []
+        lasttime = []
+
+        for reqn, request in requests:
+            v = -1
+            t = self.T
+            for i, time in enumerate(lasttime):
+                dist = self.dists[request[1]][request[3]]
+                if time + dist < request[0]:
+                    if t > time:
+                        v, t = i, time
+                
+            if v==-1:
+                trips.append([(reqn+1), -(reqn+1)])
+                lasttime.append(request[2])
+            else:
+                trips[v].extend([(reqn+1), -(reqn+1)])
+                lasttime[v] = request[2]
+        print(lasttime)
+        print(trips)
+
+        pass
     # get the chomosome's cost
--- Simulator.py
+++ Simulator.py
@@ -7,8 +7,9 @@ def main():
     print(DG)
     V = Visualization()
     V.drawPoints([coord[0] for coord in DG.stations], [coord[1] for coord in DG.stations], 'stations')
+    DG.generateOTOC()
     pass
 
 if __name__ == "__main__": # execute when python Simulator.py executed
     main()
-    print(Chromosome.generateRandomly(10,3))
+    # print(Chromosome.generateRandomly(10,3))
